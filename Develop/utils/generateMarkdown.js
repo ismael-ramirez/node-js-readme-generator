7b9a97b9d090ba621db97becadd858b520a57fdb
Ismael Ramirez <ismael-ramirez@live.com>
2020-09-15 20:44:46 -0700

    first commit

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,28 @@
-// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+  return `# **${data.title}**                 [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+  # Project Description
+  ${data.description}
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  # Installation
+  ${data.installation}
+  # Usage
+  ${data.usage}
+  # License
+  ${data.license}
+  # Contributors
+  ${data.contributors}
+  # Tests
+  ${data.tests}
+  # Questions
+  If you have any further questions feel free to contact me on Github ${data.github} or email me at ${data.email}!
 `;
 }
 
